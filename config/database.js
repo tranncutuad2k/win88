75ccd0166b7f2637fa8f959a74f9a225d81c493e
TuTA <Tuta@smac.com.vn>
2025-07-10 17:36:58 +0700

    TuTA: upcode

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'url': 'mongodb+srv://mongo-user:!23456@go88.gxpx0.mongodb.net/?retryWrites=true&w=majority&appName=go88',
+	'options': {
+		'dbName': 'CLUB3333', // red
+		//'dbName': 'GAME', // red
+		//'dbName': 'admin', // red
+                //'dbName': 'vn11022021', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
